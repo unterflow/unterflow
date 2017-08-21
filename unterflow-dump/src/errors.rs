bc2c41ddb7d773cb99cd119d6a696378ddcc8ddc
Sebastian Menski <sebastian@menski.org>
2017-08-21 18:10:26 +0200

    chore(project): extract unterflow-dump
    - now located under https://github.com/unterflow/unterflow-dump

--- unterflow-dump/src/errors.rs
+++
@@ -1,8 +0,0 @@
-error_chain! {
-    foreign_links {
-        Cli(::clap::Error);
-        Logging(::log::SetLoggerError);
-        Io(::std::io::Error);
-        Protocol(::unterflow_protocol::errors::Error);
-    }
-}
