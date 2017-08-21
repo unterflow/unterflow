676dc4f795c9d8f1f721e19af7d07654f9db6a34
Sebastian Menski <sebastian@menski.org>
2017-08-21 18:09:19 +0200

    chore(project): extract derive and protocol
    - both projects are now located under https://github.com/unterflow/unterflow-protocol

--- unterflow-protocol/src/errors.rs
+++
@@ -1,6 +0,0 @@
-error_chain! {
-    foreign_links {
-        Io(::std::io::Error);
-        Utf8Error(::std::string::FromUtf8Error);
-    }
-}
--- unterflow-protocol/src/protocol/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod client;
-pub mod gossip;
-pub mod management;
-pub mod raft;
-pub mod transport;
